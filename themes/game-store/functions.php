16fce206e044e1d1c7b012c0a1df22e33c84d950
tereshonkov <tereshonkov.dima@gmail.com>
2026-01-06 17:25:37 +0200

    Remove unused images and update theme styles and scripts for Game Store

--- themes/game-store/functions.php
+++ themes/game-store/functions.php
@@ -1,26 +1,24 @@
 <?php
-/**
- * Functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package game-store
- * @since 1.0.0
- */
 
-/**
- * Enqueue the CSS files.
- *
- * @since 1.0.0
- *
- * @return void
- */
 function game_store_styles() {
 	wp_enqueue_style(
-		'game-store-style',
-		get_stylesheet_uri(),
+		'game-general',
+		get_template_directory_uri() . '/assets/css/gamestore.css',
 		[],
 		wp_get_theme()->get( 'Version' )
 	);
+	wp_enqueue_script(
+		'gamestore-theme-related',
+		get_template_directory_uri() . '/assets/js/gamestore-theme-related.js',
+		[],
+		wp_get_theme()->get( 'Version' ),
+		true
+	);
 }
 add_action( 'wp_enqueue_scripts', 'game_store_styles' );
+
+function gamestore_google_fonts() {
+	$font_url = 'https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap';
+	wp_enqueue_style( 'gamestore-google-fonts', $font_url, [], null );
+}
+add_action( 'wp_enqueue_scripts', 'gamestore_google_fonts' );
