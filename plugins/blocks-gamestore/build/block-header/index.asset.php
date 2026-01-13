1eab4f6f2e9dc009712b2e241231c2c42b2e6665
tereshonkov <tereshonkov.dima@gmail.com>
2026-01-13 17:31:49 +0200

    notes: styles in admin menu!!!

--- plugins/blocks-gamestore/build/block-header/index.asset.php
+++ plugins/blocks-gamestore/build/block-header/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '96c3c5e89e5f9f0436de');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'fed1b9eeefe80f662154');
